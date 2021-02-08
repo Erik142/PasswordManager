374d30f45e1fa36e23e3785381936e563aa46bc2
Erik142 <EWahlberger@gmail.com>
2021-02-08 20:32:00 +0100

    Add project files

---
+++ PasswordManager/src/passwordmanager/PasswordManagerView.java
@@ -0,0 +1,5 @@
+package passwordmanager;
+
+public class PasswordManagerView {
+
+}
---
+++ PasswordManager/src/passwordmanager/Program.java
@@ -0,0 +1,10 @@
+package passwordmanager;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
