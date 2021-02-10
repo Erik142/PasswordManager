a7879fc7da7c03ac5262b25aad879c444a6b0268
Yemeri <yemeri_b@hotmail.com>
2021-02-10 23:03:13 +0100

    Sketch for a possible solution for PasswordClient

--- PasswordManager/src/passwordmanager/PasswordClient.java
+++ PasswordManager/src/passwordmanager/PasswordClient.java
@@ -1,5 +1,30 @@
 package passwordmanager;
 
 public class PasswordClient {
+//	Initiate communication with server
+	public void ActivateServerNetwork() {
+		
+	}
+	
+	
+	
+//	Checks whether Password has a value and is legal or is Null and/or illegal
+	public void CheckPasswordNotNull () {
+		
+		
+	}
+	
+	
+//	Manipulates user passwords (Store/Retrieve/Modify/Delete) using PasswordServer, UserAcc, PasswordDatabase & Credentials 
+	public void PasswordManipulation () {
+		
+	} 
+	
+//	Manipulates user account (Store/Retrieve/Modify/Delete) using UserAcc, PasswordServer and PasswordDatabase Credentials 
+	public void UserAccountManipulation () {
+		
+	}
+	
+	
 
 }
