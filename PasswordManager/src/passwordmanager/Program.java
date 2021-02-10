92c7ddc037e5e99a801134b74074ffe11f1c2735
Erik142 <EWahlberger@gmail.com>
2021-02-10 16:21:25 +0100

    Load config file on startup

--- PasswordManager/src/passwordmanager/Program.java
+++ PasswordManager/src/passwordmanager/Program.java
@@ -1,10 +1,50 @@
 package passwordmanager;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import passwordmanager.config.Configuration;
+
 public class Program {
 
+	private static final String CONFIG_PATH = "config.properties";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		URL res = Program.class.getClassLoader().getResource(CONFIG_PATH);
+		File configFile;
+		
+		try {
+			configFile = Paths.get(res.toURI()).toFile();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		Configuration config;
+		
+		try {
+			config = Configuration.GetConfiguration(configFile);
+		}
+		catch (Exception ex) {
+			System.out.println("An error occured while loading the configuration file: " + ex.getMessage());
+			System.out.println("Exiting...");
+			return;
+		}
+		
+		switch(config.appMode) {
+		case Server:
+			System.out.println("Starting new server...");
+			break;
+		case Client:
+			System.out.println("Starting new client...");
+			break;
+		case Debug:
+			System.out.println("Starting new debug configuration...");
+			System.out.println(config.toString());
+			break;
+		}
 	}
 
 }
