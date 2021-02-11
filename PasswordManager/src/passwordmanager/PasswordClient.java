0a8246ea985a472c9e6c0da31da06cc7aeb8f5ac
Yemeri <yemeri_b@hotmail.com>
2021-02-11 20:58:22 +0100

    Please review this bros!!

--- PasswordManager/src/passwordmanager/PasswordClient.java
+++ PasswordManager/src/passwordmanager/PasswordClient.java
@@ -1,26 +1,56 @@
 package passwordmanager;
 
 public class PasswordClient {
+	
+/**	
+ * 
+ *  
+ * @author yemeri 
+ *  
+ */
+	
+/** Public classes because the PasswordController has to be able to call on them **/
+	
+	
+	
+	
 //	Initiate communication with server
 	public void ActivateServerNetwork() {
 		
 	}
 	
+/**	Code below regards Login instructions for UserAccount. Send & recieve Login information **/
 	
-	
-//	Checks whether Password has a value and is legal or is Null and/or illegal
-	public void CheckPasswordNotNull () {
+	public UserAccount LoginManipulation (UserAccount C) {
 		
 		
 	}
 	
+/**	Code below regards Credentials. Manipulates user passwords (Store/Retrieve/Modify/Delete) using PasswordServer, UserAcc, PasswordDatabase & Credentials. For passwords stored inside the Password manager **/
 	
-//	Manipulates user passwords (Store/Retrieve/Modify/Delete) using PasswordServer, UserAcc, PasswordDatabase & Credentials 
-	public void PasswordManipulation () {
+	
+	public Credential GetCredential (UserAccount C) {
 		
 	} 
 	
-//	Manipulates user account (Store/Retrieve/Modify/Delete) using UserAcc, PasswordServer and PasswordDatabase Credentials 
+	
+	public boolean StoreCredential (Credential C) {
+		
+		
+	}
+	
+	public boolean ModifyCredential (Credential C) {
+		
+	}
+	
+	public boolean DeleteCredential (Credential C) {
+		
+		
+	}
+	
+/**	Manipulates user account (Store/Retrieve/Modify/Delete) using UserAcc, PasswordServer and PasswordDatabase Credentials. For account used to login **/
+//	to the password manager interface.
+	
 	public void UserAccountManipulation () {
 		
 	}
