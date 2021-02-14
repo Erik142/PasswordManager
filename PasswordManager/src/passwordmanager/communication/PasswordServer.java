1af05aeb2d3d05e4d4ad3b743415e56510357c4f
Erik142 <EWahlberger@gmail.com>
2021-02-14 16:06:14 +0100

    Implement processClient method in PasswordServer
    Worth noticing is that the getCredentials method is part of the "UserAccount events", since it takes a user account as a parameter. Also the getAccount method is currently using null values as parameters. I do not want to implement this as a special event type, so we should be able to use the UserAccount class to implement this method as well. The parameter values are null for now since the UserAccount class is not yet implemented.

--- PasswordManager/src/passwordmanager/communication/PasswordServer.java
+++ PasswordManager/src/passwordmanager/communication/PasswordServer.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import passwordmanager.Credential;
 import passwordmanager.PasswordDatabase;
 import passwordmanager.UserAccount;
+import passwordmanager.communication.CommunicationProtocol.CommunicationOperation;
 import passwordmanager.config.Configuration;
 import passwordmanager.util.StringExtensions;
 
@@ -61,7 +62,63 @@ public class PasswordServer implements Runnable {
 	}
 	
 	private void processClient(Socket client) {
+		CommunicationProtocol communicationProtocol = new CommunicationProtocol(client);
 		
+		communicationProtocol.subscribeOnSocket(new CommunicationEventListener() {
+			@Override
+			public void onCredentialEvent(Credential credential, CommunicationOperation operation) {
+				Object returnValue = null;
+				
+				switch (operation) {
+				case AddCredential:
+					returnValue = addCredential(credential);
+					break;
+				case DeleteCredential:
+					returnValue = deleteCredential(credential);
+					break;
+				case UpdateCredential:
+					returnValue = updateCredential(credential);
+				}
+				
+				if (returnValue != null) {
+					communicationProtocol.send(returnValue, operation);
+				}
+			}
+
+			@Override
+			public void onUserAccountEvent(UserAccount userAccount, CommunicationOperation operation) {
+				// TODO Auto-generated method stub
+				Object returnValue = null;
+				
+				switch (operation) {
+				case AddUser:
+					returnValue = addAccount(userAccount);
+					break;
+				case DeleteUser:
+					boolean result = true;
+					result &= deleteAllPasswords(userAccount);
+					result &= deleteAccount(userAccount);
+					returnValue = result;
+					break;
+				case GetCredential:
+					returnValue = getCredential(userAccount);
+					break;
+				case GetUser:
+					// TODO: Implement this method with actual user name and password values
+					returnValue = getAccount(null, null);
+				case UpdateUser:
+					returnValue = updateAccount(userAccount);
+					break;
+				case VerifyUser:
+					returnValue = isUserAuthorized(userAccount);
+					break;
+				}
+				
+				if (returnValue != null) {
+					communicationProtocol.send(returnValue, operation);
+				}
+			}
+		});
 	}
 	
 	private boolean addAccount(UserAccount account) {
@@ -88,7 +145,7 @@ public class PasswordServer implements Runnable {
 		return null;
 	}
 	
-	private Credential getCredential(int id) {
+	private Credential getCredential(UserAccount userAccount) {
 		return null;
 	}
 	
@@ -97,6 +154,10 @@ public class PasswordServer implements Runnable {
 	}
 	
 	private boolean updateAccount(UserAccount account) {
+		return false;
+	}
+	
+	private boolean updateCredential(Credential credential) {
 		return false;
 	}
 	
