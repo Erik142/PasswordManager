16fec806b09af4c10f4f0948859809440773f9a3
Yemeri <yemeri_b@hotmail.com>
2021-02-21 17:33:37 +0100

    Implementation of Client, Unsure about UserAccount

--- PasswordManager/src/passwordmanager/PasswordClient.java
+++ PasswordManager/src/passwordmanager/PasswordClient.java
@@ -1,6 +1,9 @@
 package passwordmanager;
 
 import passwordmanager.communication.CommunicationProtocol;
+import passwordmanager.communication.CommunicationProtocol.CommunicationOperation;
+import passwordmanager.communication.Query;
+import passwordmanager.communication.Response;
 
 public class PasswordClient {
 	
@@ -37,46 +40,69 @@ public class PasswordClient {
 /**	Code below regards Credentials. Manipulates user passwords (Store/Retrieve/Modify/Delete) using PasswordServer, UserAcc, PasswordDatabase & Credentials. For passwords stored inside the Password manager **/
 	
 	
-	public Credential getCredential (UserAccount C) {
-		return null;
+	public Credential getCredential (UserAccount account) {
+		Query<UserAccount> query = new Query<UserAccount>("", CommunicationOperation.GetCredential, account);
+		Response<Credential> response = Protocol.sendAndReceive(query);
+		return response.getData();
 		
 	} 
 	
 	
 	public boolean storeCredential (Credential credential) {
-		return false;
+		Query<Credential> query = new Query<Credential>("", CommunicationOperation.AddCredential, credential);
+		Response<Boolean> response = Protocol.sendAndReceive(query);
+		
+		return response.getData();
 		
 	}
 	
 	public boolean modifyCredential (Credential credential) {
-		return false;
+		Query<Credential> query = new Query<Credential>("", CommunicationOperation.UpdateCredential, credential);
+		Response<Credential> response = Protocol.sendAndReceive(query);
+		return response.getData() != null;
 		
 	}
 	
 	public boolean deleteCredential (Credential credential) {
-		return false;
+		Query<Credential> query = new Query<Credential>("", CommunicationOperation.DeleteCredential, credential);
+		Response<Credential> response = Protocol.sendAndReceive(query);
+		return response.getData() != null;
 		
 	}
 	
 /**	Manipulates user account (Store/Retrieve/Modify/Delete) using UserAcc, PasswordServer and PasswordDatabase Credentials. For account used to login **/
 //	to the password manager interface.
 	
-public Credential getCredential (String email, String password) {
-		return null; 
+public String verifyUser (UserAccount account) {
+	//*Not sure if Credentials is the credential for the individual or not* Not sure about the email and password part//
+	
+	Query<UserAccount> query = new Query<UserAccount>("", CommunicationOperation.VerifyUser, account);
+	Response<String> response = Protocol.sendAndReceive(query);
+	return response.getData();
+
 	} 
 	
 	
 	public boolean StoreUserAccount (UserAccount account) {
-		return false;
+		Query<UserAccount> query = new Query<UserAccount>("", CommunicationOperation.AddUser, account);
+		Response<UserAccount> response = Protocol.sendAndReceive(query);
+		return response.getData() != null;
+		
 		
 	}
 	
 	public boolean modifyUserAccount (UserAccount account) {
-		return false; 
+		
+		Query<UserAccount> query = new Query<UserAccount>("", CommunicationOperation.UpdateUser, account);
+		Response<Credential> response = Protocol.sendAndReceive(query);
+		return response.getData() != null;
 	}
 	
 	public boolean deleteUserAccount (UserAccount account) {
-		return false;
+		
+		Query<UserAccount> query = new Query<UserAccount>("", CommunicationOperation.DeleteUser, account);
+		Response<Credential> response = Protocol.sendAndReceive(query);
+		return response.getData() != null;
 		
 	}
 	
