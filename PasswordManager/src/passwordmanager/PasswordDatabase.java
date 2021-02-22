637c5db58ea50798acc133db6705763e9d19295b
Erik142 <EWahlberger@gmail.com>
2021-02-22 19:49:55 +0100

    Throw exceptions in PasswordDatabase class
    It is easier to handle these exceptions in the PasswordServer class. Then it is possible to send the correct response codes with the response messages.

--- PasswordManager/src/passwordmanager/PasswordDatabase.java
+++ PasswordManager/src/passwordmanager/PasswordDatabase.java
@@ -3,6 +3,7 @@ package passwordmanager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +17,7 @@ public class PasswordDatabase {
 	Connection c =null;
 	Statement s=null;
 	
-	PasswordDatabase(){
+	public PasswordDatabase(){
 		// Try to connect to Database
 		try {
 			
@@ -29,146 +30,93 @@ public class PasswordDatabase {
 	}
 	
 
-	public UserAccount getAccount(String Email) {
+	public UserAccount getAccount(String Email) throws SQLException {
+		this.s=c.createStatement();
+		ResultSet rs=s.executeQuery("SELECT * FROM Accounts WHERE (Email='"+Email+"')");
+		
+		while(rs.next()) { 
 			
-		try{
+			String email = rs.getString("Email");
+			String password = rs.getString("Password");
 			
-			this.s=c.createStatement();
-			ResultSet rs=s.executeQuery("SELECT * FROM Accounts WHERE (Email='"+Email+"')");
-			
-			while(rs.next()) { 
-				
-				String email = rs.getString("Email");
-				String password = rs.getString("Password");
-				
-				UserAccount acc= new UserAccount(email, password);
-				return acc;
-			}
-			
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
+			UserAccount acc= new UserAccount(email, password);
+			return acc;
 		}
+
 		return null;
-		
 	}
 	
 	
-	public void addAccount(UserAccount acc) {
-		
-		try{
-			
-			this.s=c.createStatement();
-			s.executeUpdate("INSERT INTO Accounts (Email, Password) VALUES ('"+acc.getEmail()+"','"+acc.getPassword()+"')");
-			
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
-		}
-		
+	public void addAccount(UserAccount acc) throws SQLException {
+		this.s=c.createStatement();
+		s.executeUpdate("INSERT INTO Accounts (Email, Password) VALUES ('"+acc.getEmail()+"','"+acc.getPassword()+"')");
 	}
 	
-	public void addCredential(Credential cred) {
-		
-		try{
-			
-			this.s=c.createStatement();
-			s.executeUpdate("INSERT INTO Credentials (User, URL, Username, Password) VALUES ('"+cred.getUser()+"','"+cred.getURL()+"','"+cred.getUsername()+"','"+cred.getPassword()+"')");
-			
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
-		}
-		
+	public void addCredential(Credential cred) throws SQLException {
+		this.s=c.createStatement();
+		s.executeUpdate("INSERT INTO Credentials (User, URL, Username, Password) VALUES ('"+cred.getUser()+"','"+cred.getURL()+"','"+cred.getUsername()+"','"+cred.getPassword()+"')");
 	}
 	
-	public List<Credential> listAllCredentials(UserAccount acc) {
+	public List<Credential> listAllCredentials(UserAccount acc) throws SQLException {
 		List<Credential> list=new ArrayList<>();
-		try {
-			this.s=c.createStatement();
-			ResultSet rs=s.executeQuery("SELECT URL, Username, Password FROM Credentials WHERE User='"+acc.getEmail()+"'");
-			
-			while(rs.next()) {
-				//need to return a list of credentials instead of printing it out
-				String URL=rs.getString("URL");
-				String Username=rs.getString("Username");
-				String Password=rs.getString("Password");
-				Credential cred=new Credential(acc.getEmail(), URL, Username, Password);
-				list.add(cred);
-			}
-		}catch(Exception e) {
-		System.out.println("Error: "+ e.getMessage());
+		
+		this.s=c.createStatement();
+		ResultSet rs=s.executeQuery("SELECT URL, Username, Password FROM Credentials WHERE User='"+acc.getEmail()+"'");
+		
+		while(rs.next()) {
+			//need to return a list of credentials instead of printing it out
+			String URL=rs.getString("URL");
+			String Username=rs.getString("Username");
+			String Password=rs.getString("Password");
+			Credential cred=new Credential(acc.getEmail(), URL, Username, Password);
+			list.add(cred);
 		}
+		
 		return list;
 	}
 	
-	public Credential listOneCredential(String account, String URL, String username) {
-		try {
-			this.s=c.createStatement();
-			ResultSet rs=s.executeQuery("SELECT URL, Username, Password FROM Credentials WHERE User='"+account+"' AND URL='"+URL+"' AND Username='"+username+"'");
-			while(rs.next()) {
-				//needs to return an instance of Credential instead of printing it out
-				String Url=rs.getString("URL");
-				String Username=rs.getString("Username");
-				String Password=rs.getString("Password");
-				Credential cred= new Credential(account, Url, Username, Password);
-				return cred;
-			}
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
+	public Credential listOneCredential(String account, String URL, String username) throws SQLException {
+		this.s=c.createStatement();
+		ResultSet rs=s.executeQuery("SELECT URL, Username, Password FROM Credentials WHERE User='"+account+"' AND URL='"+URL+"' AND Username='"+username+"'");
+		while(rs.next()) {
+			//needs to return an instance of Credential instead of printing it out
+			String Url=rs.getString("URL");
+			String Username=rs.getString("Username");
+			String Password=rs.getString("Password");
+			Credential cred= new Credential(account, Url, Username, Password);
+			return cred;
 		}
 		return null;
 	}
 	
-	public void deleteCredential(Credential cred) {
-		try {
-			this.s=c.createStatement();
-			s.executeUpdate("DELETE FROM Credentials WHERE User='"+cred.getUser()+"' AND URL='"+cred.getURL()+"' AND Username='"+cred.getUsername()+"'");
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
-		}
+	public void deleteCredential(Credential cred) throws SQLException {
+		this.s=c.createStatement();
+		s.executeUpdate("DELETE FROM Credentials WHERE User='"+cred.getUser()+"' AND URL='"+cred.getURL()+"' AND Username='"+cred.getUsername()+"'");
 	}
 	
-	public void deleteAllCredentials(UserAccount a) {
-		try {
-			this.s=c.createStatement();
-			s.executeUpdate("DELETE FROM Credentials WHERE User='"+a.getEmail()+"'");
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
-		}
+	public void deleteAllCredentials(UserAccount a) throws SQLException {
+		this.s=c.createStatement();
+		s.executeUpdate("DELETE FROM Credentials WHERE User='"+a.getEmail()+"'");
 	}
 	
-	public void deleteAccount(UserAccount a) {
-		try {
-			this.deleteAllCredentials(a);
-			this.s=c.createStatement();
-			s.executeUpdate("DELETE FROM Accounts WHERE Email='"+a.getEmail()+"'");
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
-		}
+	public void deleteAccount(UserAccount a) throws SQLException {
+		this.deleteAllCredentials(a);
+		this.s=c.createStatement();
+		s.executeUpdate("DELETE FROM Accounts WHERE Email='"+a.getEmail()+"'");
 	}
 	
-	public void ChangeAccountPassword(UserAccount a, String newPass) {
-		try {
-			this.s=c.createStatement();
-			s.executeUpdate("UPDATE Accounts SET Password='"+newPass+"' WHERE Email='"+a.getEmail()+"'");
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
-		}
+	public void ChangeAccountPassword(UserAccount a, String newPass) throws SQLException {
+		this.s=c.createStatement();
+		s.executeUpdate("UPDATE Accounts SET Password='"+newPass+"' WHERE Email='"+a.getEmail()+"'");
 	}
 	
-	public void ChangeCredential(Credential cred, String newPass) {
-		try {
-			this.s=c.createStatement();
-			s.executeUpdate("UPDATE Credentials SET Password='"+newPass+"' WHERE User='"+cred.getUser()+"' AND URL='"+cred.getURL()+"' AND Username='"+cred.getUsername()+"'");
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
-		}
+	public void ChangeCredential(Credential cred, String newPass) throws SQLException {
+		this.s=c.createStatement();
+		s.executeUpdate("UPDATE Credentials SET Password='"+newPass+"' WHERE User='"+cred.getUser()+"' AND URL='"+cred.getURL()+"' AND Username='"+cred.getUsername()+"'");
 	}
 		
-	public void closeConnection() {
-		try {
-			c.close();
-		}catch(Exception e) {
-			System.out.println("Error: "+ e.getMessage());
-		}
+	public void closeConnection() throws SQLException {
+		c.close();
 	}
 	
 }
