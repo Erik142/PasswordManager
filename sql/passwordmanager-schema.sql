c2b20789f1bb9e4d49175c4b56124debdff87163
Erik142 <EWahlberger@gmail.com>
2021-02-26 16:07:35 +0100

    Fix postgresql init script for Docker

--- sql/passwordmanager-schema.sql
+++ sql/passwordmanager-schema.sql
@@ -22,8 +22,6 @@ CREATE DATABASE passwordmanager
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Swedish_Sweden.1252'
-    LC_CTYPE = 'Swedish_Sweden.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
