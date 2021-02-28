08b3f88596542cb51a7596d92a48443f069da6f5
Erik142 <EWahlberger@gmail.com>
2021-02-28 18:11:30 +0100

    Add initial version of git build and tag script

---
+++ PasswordManager/build/build-and-tag.ps1
@@ -0,0 +1,48 @@
+$GIT_STATUS = $(git status -s)
+$CURRENT_BRANCH = $(git branch --show-current)
+
+if (![string]::IsNullOrEmpty($GIT_STATUS)) {
+    Write-Output "Git Status:"
+    Write-Output $GIT_STATUS
+    Write-Output "You have uncommitted changes in your git tree. Commit the changes and try again."
+    return
+}
+
+. .\version-number.ps1
+
+$OLD_SNAPSHOT = "$VERSION-SNAPSHOT"
+
+$POM_CLIENT = Get-Content ..\pom-deploy-client.xml
+$POM_SERVER = Get-Content ..\pom-deployment.xml
+$POM_DEV = Get-Content ..\pom.xml
+
+$SET_VERSION=$VERSION
+
+. .\update-version-number.ps1
+
+$NEXT_SNAPSHOT = "$VERSION-SNAPSHOT"
+
+$POM_CLIENT = $POM_CLIENT -replace $OLD_SNAPSHOT,$SET_VERSION
+Out-File -InputObject $POM_CLIENT -FilePath ..\pom-deploy-client.xml
+
+$POM_SERVER = $POM_SERVER -replace $OLD_SNAPSHOT,$SET_VERSION
+Out-File -InputObject $POM_SERVER -FilePath ..\pom-deployment.xml
+
+$COMMIT_MESSAGE="Release version $VERSION."
+
+git commit -a -m $COMMIT_MESSAGE
+git tag -a v$VERSION -m $COMMIT_MESSAGE
+
+$POM_CLIENT = $POM_CLIENT -replace $SET_VERSION,$NEXT_SNAPSHOT
+Out-File -InputObject $POM_CLIENT -FilePath ..\pom-deploy-client.xml
+
+$POM_SERVER = $POM_SERVER -replace $SET_VERSION,$NEXT_SNAPSHOT
+Out-File -InputObject $POM_SERVER -FilePath ..\pom-deployment.xml
+
+$POM_DEV = $POM_DEV -replace $OLD_SNAPSHOT,$NEXT_SNAPSHOT
+Out-File -InputObject $POM_DEV -FilePath ..\pom.xml
+
+$COMMIT_MESSAGE="Update pom files to version $NEXT_SNAPSHOT."
+
+git commit -a -m $COMMIT_MESSAGE
+git push origin/$CURRENT_BRANCH
