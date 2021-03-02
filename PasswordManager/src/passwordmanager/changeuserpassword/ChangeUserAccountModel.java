8ef93d0b8b5a72eb49eba8868929e592a5379b66
Erik142 <EWahlberger@gmail.com>
2021-03-02 20:22:59 +0100

    Add MVC for ChangePasswordDialog

---
+++ PasswordManager/src/passwordmanager/changeuserpassword/ChangeUserAccountModel.java
@@ -0,0 +1,144 @@
+package passwordmanager.changeuserpassword;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import passwordmanager.Observable;
+import passwordmanager.Observer;
+import passwordmanager.PasswordClient;
+import passwordmanager.UserAccount;
+import passwordmanager.login.LoginDialogModel;
+import passwordmanager.util.StringExtensions;
+
+public class ChangeUserAccountModel implements Observable<ChangeUserAccountModel>, Observer<LoginDialogModel> {
+
+	private final int MIN_PASSWORD_LENGTH = 8;
+	
+	private final Collection<Observer<ChangeUserAccountModel>> observers;
+	private final PasswordClient client;
+	
+	private UserAccount account;
+	
+	private String dialogMessage = "";
+	private boolean isDialogError = false;
+	
+	private boolean isViewVisible = false;
+	
+	private String oldPassword = "";
+	private String confirmPassword = "";
+	private String newPassword = "";
+	
+	public ChangeUserAccountModel(PasswordClient client) {
+		this.observers = new HashSet<Observer<ChangeUserAccountModel>>();
+		this.client = client;
+	}
+	
+	public void changeUserPassword(String oldPassword, String newPassword, String confirmPassword) {
+		if (isValidPassword(newPassword) && isValidPassword(confirmPassword) && newPassword.equals(confirmPassword)) {
+			
+			if (oldPassword.equals(account.getPassword())) {
+				boolean success = client.modifyUserAccount(account);
+				
+				if (success) {
+					dialogMessage = "Successfully changed the user password!";
+					isDialogError = false;
+				} else {
+					dialogMessage = "The user password could not be updated. Try again later.";
+					isDialogError = true;
+				}
+			} else {
+				this.dialogMessage = "The old password is not correct!";
+				isDialogError = true;
+			}
+		}
+		else if (!isValidPassword(newPassword) || !isValidPassword(confirmPassword)) {
+			this.dialogMessage = "The password must be at least 8 characters long!";
+			this.isDialogError = true;
+		}
+		else {
+			dialogMessage = "Password cannot be empty!";
+			isDialogError = true;
+		}
+		
+		updateObservers();
+	}
+	
+	public String getDialogMessage() {
+		String dialogMessage = this.dialogMessage;
+		
+		this.dialogMessage = "";
+		
+		return dialogMessage;
+	}
+	
+	public boolean getIsDialogError() {
+		return this.isDialogError;
+	}
+	
+	public boolean getIsViewVisible() {
+		return this.isViewVisible;
+	}
+	
+	public String getNewPassword() {
+		return this.newPassword;
+	}
+	
+	public String getOldPassword() {
+		return this.oldPassword;
+	}
+	
+	public String getConfirmPassword() {
+		return this.confirmPassword;
+	}
+	
+	private boolean isValidPassword(String password) {
+		return !StringExtensions.isNullOrEmpty(password) && password.trim().length() >= MIN_PASSWORD_LENGTH;
+	}
+	
+	public void setAccount(UserAccount account) {
+		this.account = account;
+		
+		updateObservers();
+	}
+	
+	public void setIsViewVisible(boolean isVisible) {
+		this.isViewVisible = isVisible;
+		
+		updateObservers();
+	}
+	
+	public void reset() {
+		this.oldPassword = "";
+		this.newPassword = "";
+		this.confirmPassword = "";
+		this.dialogMessage = "";
+		this.isDialogError = false;
+		
+		updateObservers();
+	}
+	
+	private void updateObservers() {
+		for (Observer<ChangeUserAccountModel> observer : observers) {
+			observer.update(this);
+		}
+	}
+	
+	@Override
+	public void addObserver(Observer<ChangeUserAccountModel> observer) {
+		this.observers.add(observer);
+	}
+
+	@Override
+	public void removeObserver(Observer<ChangeUserAccountModel> observer) {
+		this.observers.remove(observer);
+	}
+
+	@Override
+	public void update(LoginDialogModel observable) {
+		String email = observable.getEmail();
+		String password = observable.getPassword();
+		
+		setAccount(new UserAccount(email, password));
+	}
+
+}
