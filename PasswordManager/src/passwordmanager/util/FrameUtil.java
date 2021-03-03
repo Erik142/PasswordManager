9e6d9c4c638ff4ef3a2123e2542bb4cf2a1bf8e3
Erik142 <EWahlberger@gmail.com>
2021-03-03 09:49:29 +0100

    Add FrameUtil class

---
+++ PasswordManager/src/passwordmanager/util/FrameUtil.java
@@ -0,0 +1,22 @@
+package passwordmanager.util;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class FrameUtil {
+	/***
+	 * Positions the <code>JFrame</code> in the center of the screen.
+	 * @param frame The <code>JFrame</code> to be centered
+	 */
+	public static void centerFrame(JFrame frame) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension windowSize = frame.getSize();
+		
+		int windowXPos = (int)(screenSize.getWidth() / 2 - windowSize.getWidth() / 2);
+		int windowYPos = (int)(screenSize.getHeight() / 2 - windowSize.getHeight() / 2);
+		
+		frame.setBounds(windowXPos, windowYPos, (int)windowSize.getWidth(), (int)windowSize.getHeight());
+	}
+}
