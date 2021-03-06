9672716ead09361f1790b650a612239b212293d0
Erik142 <EWahlberger@gmail.com>
2021-03-06 22:18:17 +0100

    Add JavaDoc for ModelException class

--- PasswordManager/src/passwordmanager/exception/ModelException.java
+++ PasswordManager/src/passwordmanager/exception/ModelException.java
@@ -1,10 +1,13 @@
 package passwordmanager.exception;
 
+/**
+ * Custom Exception class used to throw error messages in MVC Models
+ */
 public class ModelException extends Exception {
-    public ModelException() {
-        super();
-    }
-
+    /**
+     * Creates a new instance of a ModelException object, with the specified message String
+     * @param message The message String
+     */
     public ModelException(String message) {
         super(message);
     }
